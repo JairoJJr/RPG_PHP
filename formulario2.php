a3dd870da53f06882b22ab697f57b83956216187
JairoJJr <jairojannini.contato@gmail.com>
2023-10-08 21:21:53 -0300

    segunda pag
    Ainda estou tentando colocar os botões de mais e menos nos atributos...

---
+++ formulario2.php
@@ -0,0 +1,36 @@
+<?php 
+    $nome = $_POST["nome"];
+    $idade = $_POST["idade"];
+    $raca = $_POST["raca"];
+    $classe = $_POST["classe"];
+    $obs = $_POST["obs"];
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atributos</title>
+    <!--<link rel="stylesheet" href="style.css">-->
+</head>
+<body>
+    <header>
+        <h1>Atributos</h1>
+        <form action="SeuPersonagem.php" method="POST">
+            <input type="hidden" name="nome" value="<?php echo $nome ?>">
+            <input type="hidden" name="idade" value="<?php echo $idade ?>">
+            <input type="hidden" name="raca" value="<?php echo $raca ?>">
+            <input type="hidden" name="classe" value="<?php echo $classe ?>">
+
+            <label for="forca">Força</label>
+            <div>
+                <button style="display: inline-block;">-</button>
+                <button style="display: inline-block;">+</button>
+            </div>
+                <input type="submit" value="Gerar" name="gerar">
+            
+        </form>
+    </header>
+</body>
+</html>
