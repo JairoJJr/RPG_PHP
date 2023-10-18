dbd89c3943a98ab90d9ea80749abf9ad3a04a534
JairoJJr <jairojannini.contato@gmail.com>
2023-10-18 14:17:51 -0300

    formulario-prompt
    formulárioa já está com os campos solicitados, e arrumei uns detalhes do prompt

--- prompt.php
+++ prompt.php
@@ -6,6 +6,10 @@
     $tracos_de_personalidade = $_POST['tracos_de_personalidade'];
     $raca = $_POST['raca'];
     $classe = $_POST['classe'];
+    $tipo_cabelo = $_POST['tipo_cabelo'];
+    $cor_cabelo = $_POST['cor_cabelo'];
+    $acessorio_aparencia = $_POST['acessorio_aparencia'];
+    $tema_background = $_POST['tema_background'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -28,7 +32,7 @@
     ?></p>
     <!--Testando Prompt -->
     <p><?php 
-        $prompt = "Crie uma história para meu personagem de D&D para uma campanha que vou iniciar. O personagem se chama $nome, tem $idade anos de idade, é da raça $raça, da classe $classe, é do sexo $sexo, seu traço de personalidade é $tracos__de_personalidade, a história será de <tema_background>. Para preenchimento da ficha, com base na história e personalidade dele, também crie frases de até 100 caracteres para os campos de: Traços de personalidade, Ideais, Vínculos e Fraquezas. Suas regras para criar a história são: Não especifique o local de nascimento e nem dê nomes de lugares, apenas características sobre o lugar; Só escreva a história anterior à campanha que eu iniciarei agora, sem fazer nenhum tipo de introdução à jornada que farei no jogo; Não faça afirmações do tipo \"moral da história\".
+        $prompt = "Crie uma história para meu personagem de D&D para uma campanha que vou iniciar. O personagem se chama $nome, tem $idade anos de idade, é da raça $raça, da classe $classe, é do sexo $sexo, seu traço de personalidade é $tracos__de_personalidade, a história será de $tema_background. Seu cabelo é $cor_cabelo e do tipo $tipo_cabelo. Possui $acessorio_aparencia. Para preenchimento da ficha, com base na história e personalidade dele, também crie frases de até 100 caracteres para os campos de: Traços de personalidade, Ideais, Vínculos e Fraquezas. Suas regras para criar a história são: Não especifique o local de nascimento e nem dê nomes de lugares, apenas características sobre o lugar; Só escreva a história anterior à campanha que eu iniciarei agora, sem fazer nenhum tipo de introdução à jornada que farei no jogo; Não faça afirmações do tipo \"moral da história\".
         Com base nesse personagem preencha os pontos de atributos que são: força, destreza, constituição, inteligência, sabedoria e carisma.";
         echo $prompt;
     ?></p>
