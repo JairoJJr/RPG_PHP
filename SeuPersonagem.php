447139c7697f82fcbd540f55503f3f7e4da5f78a
JairoJJr <jairojannini.contato@gmail.com>
2023-10-19 01:19:56 -0300

    integração com chat ok
    Falta colocar uma conta

--- SeuPersonagem.php
+++ SeuPersonagem.php
@@ -13,34 +13,39 @@
             require 'prompt.php';
             echo $prompt;
 
-            $apiKey = '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';
+            $apiKey = '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';
 
             $endPoint = 'https://api.openai.com/v1/chat/completions';
 
-            $dados = [
+            $dados = json_encode(array(
                 'model' => 'gpt-3.5-turbo',
-                'messages' => ['role'=>'user', 'content'=>$prompt]
-            ];
+                'messages' => [
+                    ['role' => 'user', 'content' => "$prompt"],
+                ]
+                ));
 
-            $ch = curl_init($endPoint);
+            $ch = curl_init();
 
+            curl_setopt($ch, CURLOPT_URL, $endPoint);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_HTTPHEADER, [
-                'Authorization: Bearer ' . $apiKey,
                 'Content-Type: application/json',
+                'Authorization: Bearer ' . $apiKey,                
             ]);
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($dados));
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $dados);
 
             $response = curl_exec($ch);
 
             if($response === false) {
                 echo 'Deu Ruim!' . curl_error($ch);
             }else {
-                $resultado = json_decode($response);
-                echo $resultado->choices[0]->text;
+                $resultado = json_decode($response,true);
+                echo '<div>';
+                echo var_dump($resultado);
+                echo '<div>';
             }
-            //curl_close($ch);
+            curl_close($ch);
             ?></p>
     </body>
 </html>
